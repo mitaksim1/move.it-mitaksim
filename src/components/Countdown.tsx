5c10ccf44a054103d8801a6a1c51690d3ab7081d
mitaksim1 <miriamt.simonnet@gmail.com>
2021-02-23 19:03:03 +0100

    Explication de comment ça marche useEffect() avec un exemple simple

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,30 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import styles from '../styles/components/Countdown.module.css';
 
 export function Countdown() {
     // Initialization du chronometre en secondes
     const [time, setTime] = useState(25 * 60);
+    // Ce state vérifie si le countdown est en marche ou arrêté
+    // On l'initialise à false, parce qu'il faut le clic de l'utilisateur pour le mettre en marche
+    const [active, setActive] = useState(false);
 
     // Pour trouver les minutes 
     // Math.floor() : arrondi à l'entier plus petit
     const minutes = Math.floor(time / 60);
 
-    // 
     const secondes = time % 60;
-
-    /* String(minutes) : transforme la valeur de minutes en string. Ex : 25 = '25' 
-    .padStart(2, '0') : on veut deux strings, s'il n'y qu'un on ajoute un 0 à gauche. Ex: On a que 5 minutes, alors la fonction padStart() va ajouter un 0 à 5 et on va se retrouver avec 05 comme ça on pourra faire un split.
-    .split('') : sépare la string en deux parties et nous retourne un array avec ces deux parties. Ex: 25 -> devient '2' '5' -> ['2', '5'] 
-
-    const minuteLeftRight = String(minutes).padStart(2, '0').split('');
-    */ 
 
     // Comme on nous retourne un array, on peut la destructurer 
     const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
 
     // On fait la même chose avec les secondes 
     const [secondeLeft, secondeRight] = String(secondes).padStart(2, '0').split('');
+
+    // Au clic sur le bouton, on appelle cette fonction
+    function startCountdown() {
+        // qui va activer le chronometre et va le lancer
+        setActive(true);
+    }
+
+    /* useEffect() prend deux paramètres : le premier est toujours une fonction, le deuxième c'est quand je veux que cette fonction soit exécutée.
+    Dans cet exemple, si on met le code comme suit, cette fonction sera exécutée à chaque fois qu'il y aura un changement dans la variable active. Elle commence à false, mais si on clique sur le bouton elle passera à true.
+    Ce sera ici alors, qu'il faudra mettre le décomptage du chronometre après le clique sur le bouton et l'activation du state du coup.
+    */
+    useEffect(() => {
+        console.log(active)
+    }, [active]);
 
     return (
         <div>
@@ -40,7 +49,11 @@ export function Countdown() {
                     <span>{secondeRight}</span>
                 </div>
             </div>
-            <button type="button" className={styles.countdownButton}>
+            <button 
+                type="button" 
+                className={styles.countdownButton}
+                onClick={startCountdown}
+            >
                 Lancer un cycle
             </button>
         </div>   
