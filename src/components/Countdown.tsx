b756de64037c83484913074ed4f07336a7f56d22
mitaksim1 <miriamt.simonnet@gmail.com>
2021-02-23 18:07:55 +0100

    Création du bouton de lancement du chronometre et ses styles css

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -2,17 +2,22 @@ import styles from '../styles/components/Countdown.module.css';
 
 export function Countdown() {
     return (
-        <div className={styles.countdownContainer}>
-            <div>
-                {/* On a séparé le chiffre 25 en deux span pour faciliter la stylisation */}
-                <span>2</span>
-                <span>5</span>
+        <div>
+            <div className={styles.countdownContainer}>
+                <div>
+                    {/* On a séparé le chiffre 25 en deux span pour faciliter la stylisation */}
+                    <span>2</span>
+                    <span>5</span>
+                </div>
+                <span>:</span>
+                <div>
+                    <span>0</span>
+                    <span>0</span>
+                </div>
             </div>
-            <span>:</span>
-            <div>
-                <span>0</span>
-                <span>0</span>
-            </div>
-        </div>
+            <button type="button" className={styles.countdownButton}>
+                Lancer un cycle
+            </button>
+        </div>   
     );
 }
