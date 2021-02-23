c78026896db9a302d1f689c96b1627f32c6653fc
mitaksim1 <miriamt.simonnet@gmail.com>
2021-02-23 17:56:47 +0100

    Ajout titre de la page home et ajout du favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,19 @@
-import Head from 'next/head';
 import { ExperienceBar } from '../components/ExperienceBar';
-import { Profile } from '../components/Profile';
-import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
+import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Profile } from '../components/Profile';
+
+import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
+
       <ExperienceBar />
       <section>
         {/* Partie gauche */}
