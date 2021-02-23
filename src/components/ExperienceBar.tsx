db38a702f8884e6c9552cdec94dfba2c62d6e3b7
mitaksim1 <miriamt.simonnet@gmail.com>
2021-02-23 15:08:06 +0100

    Factorisation du style css du composant ExperienceBar dans un fichier à part utilisant le module.css de Next

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,12 +1,15 @@
+import styles from '../styles/components/ExperienceBar.module.css';
+
 export function ExperienceBar() {
     return (
-        <header className="experience-bar">
+        /* La façon d'appeler un module.css */
+        <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div> 
                 {/* On a laissé le style ici, parce que c'est une variable qui va changer, contrairement aux styles dans le fichier css */}
                 <div style={{ width: '50%'}}/>
 
-                <span className="current-experience" style={{ left: '50%' }}>
+                <span className={styles.currentExperience} style={{ left: '50%' }}>
                     300 xp
                 </span>
             </div>
