2e8e225b884f52b68bb06948d123fe28c6c60132
mitaksim1 <miriamt.simonnet@gmail.com>
2021-02-23 15:31:19 +0100

    Création du composant du profil de l'utilisateur plus styles css de ce composant

---
+++ src/components/Profile.tsx
@@ -0,0 +1,14 @@
+import styles from '../styles/components/Profile.module.css';
+
+// Après la création de ce composant, on pourra l'appeler dans index.tsx
+export function Profile() {
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/diego3g.png" alt="Diego Fernandes"/>
+            <div>
+                <strong>Diego Fernandes</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,23 @@
 import Head from 'next/head';
 import { ExperienceBar } from '../components/ExperienceBar';
+import { Profile } from '../components/Profile';
+
+import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <ExperienceBar />
+      <section>
+        {/* Partie gauche */}
+        <div>
+          <Profile />
+        </div>
+        {/* Partie droite */}
+        <div>
+
+        </div>
+      </section>
     </div>
   )
 }
