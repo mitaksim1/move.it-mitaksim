1f3b2b07257a83d2fe1b5ea03fa162f242e60a54
mitaksim1 <miriamt.simonnet@gmail.com>
2021-02-23 14:47:57 +0100

    Structure de base de l'application utilisant Next.js

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+// Tout ce qu'est static reste dans ce fichier
+
+import '../styles/global.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
