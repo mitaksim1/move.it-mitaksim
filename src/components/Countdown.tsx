4037812f5cbce6f0f23c9f127869be9302bee959
mitaksim1 <miriamt.simonnet@gmail.com>
2021-02-23 17:49:35 +0100

    Création composant Countdown et ses styles css

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,18 @@
+import styles from '../styles/components/Countdown.module.css';
+
+export function Countdown() {
+    return (
+        <div className={styles.countdownContainer}>
+            <div>
+                {/* On a séparé le chiffre 25 en deux span pour faciliter la stylisation */}
+                <span>2</span>
+                <span>5</span>
+            </div>
+            <span>:</span>
+            <div>
+                <span>0</span>
+                <span>0</span>
+            </div>
+        </div>
+    );
+}
