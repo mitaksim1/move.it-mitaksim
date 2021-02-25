d913f09514a6f03efd22fdc4fff62b032e20d0fe
mitaksim1 <miriamt.simonnet@gmail.com>
2021-02-25 19:59:04 +0100

    Appel au context dans le composant CompletedChallenges et dynamisations de la donnée

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -6,9 +6,7 @@ import styles from '../styles/components/ExperienceBar.module.css';
 export function ExperienceBar() {
     const { currentExperience, experienceToNextLevel } = useContext(ChallengesContext);
 
-    // 1. Gére le pourcentage sur la barre de niveau 
-    // Selon le calcul fait, les points maxi sont de 64 xp, alors les points du milieu de la barre :
-    // On a ajouté Math.round pour avoir un chiffre rond et ne pas avoir des soucis au nivaeu des styles
+    // Gére le pourcentage sur la barre de niveau 
     const percentToNextLevel = Math.round(currentExperience * 100) / experienceToNextLevel;
 
     return (
@@ -17,7 +15,6 @@ export function ExperienceBar() {
             <span>0 xp</span>
             <div>  
                 {/* On a laissé le style ici, parce que c'est une variable qui va changer, contrairement aux styles dans le fichier css */}
-                {/* 2. On dynamise les valeurs mis en brut par la nouvelle variable */}
                 <div style={{ width: `${percentToNextLevel}%`}}/>
                 <span className={styles.currentExperience} style={{ left: `${percentToNextLevel}%` }}>
                     {currentExperience} xp
