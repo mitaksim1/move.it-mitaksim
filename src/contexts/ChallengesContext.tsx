fe6699503de04e6170b9f5bcb22d4d284e3e2186
mitaksim1 <miriamt.simonnet@gmail.com>
2021-02-25 16:49:29 +0100

    Interface pour préciser le type attendu dans children

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -2,9 +2,16 @@ import { createContext, useState, ReactNode } from 'react';
 
 export const ChallengesContext = createContext({});
 
+// On va préciser le type attendu pour le children 
+// Comme le children n'est pas juste un text mais aussi un composant React, on utilise ReactNode qui nous permettra d'accepter n'importe quel type de données comme children (texte, html, composant)
+interface ChallengesProviderProps {
+    children: ReactNode;
+}
+
 // On va importer cette fonction dans _app
 // On destructure la récéption du paramètre props.children (<Component {...pageProps} />)
-export function ChallengesProvider({ children }) {
+// Après la création du type attendue dans children : 
+export function ChallengesProvider({ children }: ChallengesProviderProps) {
     const [level, setlevel] = useState(1);
 
     function levelUp() {
