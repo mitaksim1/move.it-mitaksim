d333d649b16864638854ed3587c0a423de0211b6
mitaksim1 <miriamt.simonnet@gmail.com>
2021-02-26 10:16:47 +0100

    Appel à context dans Profile pour dynamiser la valeur de level

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,12 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext'
+
 import styles from '../styles/components/Profile.module.css';
 
 // Après la création de ce composant, on pourra l'appeler dans index.tsx
 export function Profile() {
+    // Appel à context
+    const { level } = useContext(ChallengesContext);
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/diego3g.png" alt="Diego Fernandes"/>
@@ -9,7 +14,7 @@ export function Profile() {
                 <strong>Diego Fernandes</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level"/>
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
