18511974d863f1d48317685c24d554a724877fb2
mitaksim1 <miriamt.simonnet@gmail.com>
2021-02-27 15:41:02 +0100

    Appel au context dans le fichier du Modal pour dynamiser les données

--- src/components/LevelUpModal.tsx
+++ src/components/LevelUpModal.tsx
@@ -1,11 +1,17 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/LevelUpModal.module.css';
 
 
+
 export function LevelUpModal() {
+    // Pour pouvoir accèder aux données de level et pouvoir dynamiser la valeur du header, on appelle le context
+    const { level } = useContext(ChallengesContext);
     return (
         <div className={styles.overlay}>
             <div className={styles.container}>
-                <header>2</header>
+                <header>{level}</header>
 
                 <strong>Bravo!</strong>
                 <p>Tu as atteint un niveau de plus</p>
